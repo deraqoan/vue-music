9840b77f4f817ebaee32ce16add3a15eb674b7f7
deraqoan <book@bookdeMacBook-Pro.local>
2017-08-04 04:31:23 +0800

    vue模仿qq音乐

---
+++ src/base/nav/nav.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tab">
+    <router-link tag="div" class="tab-item" to="/loading">
+      <span class="tab-link">音乐盒</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" to="/loading">
+      <span class="tab-link">客户端</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" to="/loading">
+      <span class="tab-link">注册
+      </span>
+    </router-link>
+    <router-link tag="div" class="tab-item" to="/search">
+      <span class="tab-link">登录</span>
+    </router-link>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  .tab
+    display: flex
+    height: 44px
+    width:800px
+    margin:0 auto
+    line-height: 44px
+    font-size: $font-size-medium
+    .tab-item
+      flex: 1
+      text-align: center
+      .tab-link
+        padding-bottom: 5px
+        color: $color-text-l
+      &.router-link-active
+        .tab-link
+          color: $color-theme
+          border-bottom: 2px solid $color-theme
+</style>
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+    <h1 class="text">Chicken Music</h1>
+    <router-link tag="div" class="mine" to="/user">
+      <i class="icon-mine"></i>
+      </router-link>
+  
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin"
+  .m-header
+    position: relative
+    height: 44px
+    color: #261d1d
+    font-size: 0
+    background:#ffdede
+    .icon
+      display: inline-block
+      vertical-align: top
+      margin-top: 6px
+      width: 30px
+      height: 32px
+      margin-right: 9px
+      margin-left: 30px
+      bg-image('logo')
+      background-size: 30px 32px
+    .text
+      display: inline-block
+      vertical-align: top
+      line-height: 44px
+      font-size: $font-size-large
+    .tab
+      display: flex
+      height: 30px
+      width:200px
+      position: absolute
+      top:6px
+      right: 200px
+      line-height: 30px
+      font-size: $font-size-large
+      .tab-item
+        flex: 1
+        text-align: center
+        margin-left:30px
+        border:1px solid $color-theme 
+        border-radius: 5px;
+        .tab-link
+          padding-bottom: 5px
+          color: $color-text-l
+        &.router-link-active
+          .tab-link
+            color: $color-theme
+            background:#ccc
+    .mine
+      position: absolute
+      top: 0
+      right: 40px
+      .icon-mine
+        display: block
+        padding: 12px
+        font-size: 20px
+        color: $color-theme
+</style>
